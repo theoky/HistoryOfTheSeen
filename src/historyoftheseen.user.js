5f94c9f8711e647681b2a573252d77ecbd4ba126
theoky <theoky.ctxt@gmail.com>
2014-08-30 01:00:41 +0200

    new urls, dot notation, changed dimming, hide more elements depending on site

--- src/historyoftheseen.user.js
+++ src/historyoftheseen.user.js
@@ -3,11 +3,11 @@
 // @namespace https://github.com/theoky/HistoryOfTheSeen
 // @description Script to implement a history of the seen approach for some news sites. Details at https://github.com/theoky/HistoryOfTheSeen
 // @author          Theoky
-// @version	        0.36
-// @lastchanges     Menu: Remove info for domain
+// @version	        0.39
+// @lastchanges     new urls, dot notation, changed dimming, hide more elements
 // @license         GNU GPL version 3
 // @released        2014-02-20
-// @updated         2014-08-28
+// @updated         2014-08-30
 // @homepageURL   	https://github.com/theoky/HistoryOfTheSeen
 //
 // @grant      GM_getValue
@@ -42,6 +42,7 @@
 // @include http*://kurier.at/*
 // @include http*://slashdot.org/*
 // @include http*://taz.de/*
+// @include http*://notalwaysright.com/
 
 // @require https://greasyfork.org/scripts/130-portable-md5-function/code/Portable%20MD5%20Function.js?version=10066
 // was require md5.js 
@@ -70,24 +71,52 @@
 
 //(function(){
 
-var defaultSettings = {
+	var defaultSettings = {
 			ageOfUrl: 5, 	// age in days after a url is deleted from the store
 							// < 0 erases all dates (disables history)
 			targetOpacity: 0.3,
+			targetOpacity4Dim: 0.65,
 			steps: 10,
 			dimInterval: 30000,
 			expireAllDomains: true		// On fast machines this can be true and expires
 							// all domains in the database with each call. If false,
 							// only the urls of the current domain are expired which 
 							// is slightly faster. 
-	}
+		}
+
+	var perUrlSettings = [
+		{
+			url : '.*\.?derstandard\.at',
+			// TODO: der, die, das standard
+			parentHints : [
+					"ancestor::div[contains(concat(' ', @class, ' '), ' text ')]",
+					"ancestor::ul[@class='stories']" ]
+		},
+
+		{
+			url : 'notalwaysright\.com',
+			parentHints : [ "ancestor::div[contains(concat(' ', @class, ' '), ' post ')]" ]
+		},
+
+		{
+			url : '.*\.?golem.de',
+			parentHints : [ "ancestor::li",
+					"ancestor::section[@id='index-promo']",
+					"ancestor::section[contains(concat(' ', @class, ' '), ' promo ')]" ]
+		},
+
+		{
+			url : '.*\.?reddit.com',
+			parentHints : [ "ancestor::div[contains(concat(' ', @class, ' '), ' thing ')]" ]
+		}
+	]
 	
 	var dimMap = {};
-	var countDownTimer = defaultSettings["steps"];
+	var countDownTimer = defaultSettings.steps;
 	var theHRefs = null;
+	var curSettings = null;
 
-	function resetAllUrls()
-	{
+	function resetAllUrls() {
 		if (confirm('Are you sure you want to erase the complete seen history?')) {
 			var keys = GM_listValues();
 			for (var i=0, key=null; key=keys[i]; i++) {
@@ -113,46 +142,49 @@ var defaultSettings = {
 		}
 	}
 	
-	function resetUrlsForCurrentDomain()
-	{
+	function resetUrlsForCurrentDomain() {
 		resetUrlsForCurrentHelper("domain", document.domain)
 	}
 	
-	function resetUrlsForCurrentSite()
-	{
+	function resetUrlsForCurrentSite() {
 		resetUrlsForCurrentHelper("base", document.baseURI)
 	}
 
-	function expireUrlsForCurrentSite()
-	{
+	function expireUrlsForCurrentSite()	{
+		// TODO: do this only once a day
 		var keys = GM_listValues();
 		if (!keys) {
 			return;
 		}
 
-		var cutOffDate = calcCutOffDate(defaultSettings["ageOfUrl"]);
+		var cutOffDate = calcCutOffDate(defaultSettings.ageOfUrl);
 
 		for (var i=0, key=null; key=keys[i]; i++) {
+			if (key == "lastExpire"){
+				continue;
+			}
+				
 			var dict = JSON.parse(GM_getValue(key, "{}"));
 			
 			if(dict) {
 				try {
 					// console.log(dict["domain"], cutOffDate.getTime(), dict["date"]);
 					if (cutOffDate.getTime() > dict["date"]) {
-						if (defaultSettings["expireAllDomains"] ||
+						if (defaultSettings.expireAllDomains ||
 							(dict["domain"] == document.domain))
 						{
 							GM_deleteValue(key);
 						}
 					}
 				} catch (e) {
-					GM_log(e);
+					console.log(e);
 				}
 			}
 			else {
-				GM_log('Error! JSON.parse failed - dict is likely to be corrupted.');
+				console.log('Error! JSON.parse failed - dict is likely to be corrupted.');
 			}
 		}
+		GM_setValue("lastExpire", new Date());
 	}
 
 	function calcCutOffDate(age) {
@@ -163,6 +195,51 @@ var defaultSettings = {
 		}
 		return cutOffDate;
 	}
+	
+	function findPerUrlSettings(theSettings, aDomain) {
+//		console.log("findPerUrlSettings", aDomain);
+		for (var i=0; i < theSettings.length; ++i) {
+			var myRegExp = new RegExp(theSettings[i].url, 'i');
+			if (aDomain.match(myRegExp)) {
+				return theSettings[i];
+			}
+		}
+	}
+
+	function locateParentElem(curSettings, aDomain, aRoot) {
+		if (!curSettings) {
+			return null;
+		}
+		// console.log("locateParentElem 1", curSettings.url);
+		var res = null;
+		for (xpath = 0; xpath < curSettings.parentHints.length; ++xpath) {
+			// console.log("locateParentElem 2", curSettings.parentHints[xpath], aRoot);
+			res = document.evaluate(curSettings.parentHints[xpath], aRoot, null, 9, null).singleNodeValue
+			if (res) {
+				// console.log("locateParentElem found something");
+				return res;
+			}
+		}
+		return res;
+	}
+	
+	function dimLinks() {
+		interval = (1 - defaultSettings.targetOpacity4Dim)/defaultSettings.steps;
+		countDownTimer = countDownTimer - 1;
+		curOpacity = defaultSettings.targetOpacity4Dim + interval*countDownTimer;
+
+		for(var i = 0; i < theHRefs.length; i++)
+		{
+			var hash = 'm' + hex_md5(theHRefs[i].href);
+			if (hash in dimMap) {
+				theHRefs[i].style.opacity = curOpacity;
+			}
+		}
+		
+		if (countDownTimer > 0) {
+			to = setTimeout(dimLinks, defaultSettings.dimInterval);
+		}
+	}
 
 //	Main part
 
@@ -171,17 +248,21 @@ var defaultSettings = {
 	GM_registerMenuCommand("Remove the seen history for this domain.", resetUrlsForCurrentDomain);
 	GM_registerMenuCommand("Remove all seen history (for all sites)!", resetAllUrls);
 
-	function run_script(){
+	function run_script() {
 		// Vars
 		var allHrefs = document.getElementsByTagName("a");
 		var theBase = document.baseURI;
 		var theDomain = document.domain;
 		var elemMap = {};
+		
 		dimMap = {};
+
+		curSettings = findPerUrlSettings(perUrlSettings, theDomain);
+		// console.log(curSettings);
 
 		// expire old data
 		expireUrlsForCurrentSite();
-		
+
 		// Change the DOM
 
 		// First loop: gather all new links and make already seen opaque.
@@ -195,8 +276,16 @@ var defaultSettings = {
 
 			if (typeof key != 'undefined') {
 				// key found -> loaded this reference already 
-				// change display
-				allHrefs[i].style.opacity = defaultSettings["targetOpacity"];
+				
+				pe = locateParentElem(curSettings, theDomain, allHrefs[i])
+				// console.log("locate parent done", pe);
+				if (pe) {
+					pe.style.opacity = defaultSettings.targetOpacity;
+				} else {
+					// change display
+					allHrefs[i].style.opacity = defaultSettings.targetOpacity;
+				}
+				
 			} else {
 				// key not found, store it with current date
 				elemMap[hash] = {"domain":theDomain, "date":(new Date()).getTime(), "base":theBase};
@@ -210,25 +299,7 @@ var defaultSettings = {
 		}
 		
 		theHRefs = allHrefs;
-		to = setTimeout(dimLinks, defaultSettings["dimInterval"]);
-	}
-
-	function dimLinks() {
-		interval = (1 - defaultSettings["targetOpacity"])/defaultSettings["steps"];
-		countDownTimer = countDownTimer - 1;
-		curOpacity = defaultSettings["targetOpacity"] + interval*countDownTimer;
-
-		for(var i = 0; i < theHRefs.length; i++)
-		{
-			var hash = 'm' + hex_md5(theHRefs[i].href);
-			if (hash in dimMap) {
-				theHRefs[i].style.opacity = curOpacity;
-			}
-		}
-		
-		if (countDownTimer > 0) {
-			to = setTimeout(dimLinks, defaultSettings["dimInterval"]);
-		}
+		to = setTimeout(dimLinks, defaultSettings.dimInterval);
 	}
 
 	run_script();
